1021da28f89f21629ef4705c1e4e631fafe7ab37
Leandro Quiroga <leandroquiroga9514@gmail.com>
2022-08-12 14:43:58 -0300

    route: Has been fixed middleware that control mongoID

---
+++ routes/categories-routes.js
@@ -0,0 +1,63 @@
+const {Router} = require('express');
+const {check} = require('express-validator');
+
+const { validationField } = require('../middlewares/validatorError');
+const { validatorJWT } = require('../middlewares/validatorJWT');
+const { categoryIDExist } = require('../helpers/db-validators');
+
+const { getCategories, 
+        getCategoryByID, 
+        addCategory, 
+        updateCategory, 
+        deleteCategory 
+      } = require('../controllers/categories-controllers');
+const { isAdminRole } = require('../middlewares/validatorRole');
+
+const categories = Router();
+
+// Get all categories - public
+categories.get('/', getCategories );
+
+// Get one category by id - public
+categories.get('/:id',
+  [
+    check('id', 'ID Invalid').isMongoId(),
+    check('id').custom(categoryIDExist),
+    validationField
+  ],
+  getCategoryByID 
+);
+
+// Add a new category - private 
+categories.post('/', 
+  [
+    validatorJWT,
+    check('name', 'Please enter a name').not().isEmpty(),
+    validationField,
+  ], 
+  addCategory
+);
+
+// Update a category
+categories.put('/:id', 
+  [
+    validatorJWT,
+    check('id', 'ID Invalid').isMongoId(),
+    check('id').custom(categoryIDExist),
+    validationField
+  ],
+  updateCategory 
+);
+
+// Delete a category
+categories.delete('/:id', 
+  [
+    validatorJWT,
+    isAdminRole,
+    check('id', 'ID Invalid').isMongoId(),
+    check('id').custom(categoryIDExist),
+    validationField
+  ],
+  deleteCategory 
+);
+module.exports = categories;
